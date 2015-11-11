8c18f5ae0a03cabc7ac0f6b53236e44c042f384b
a56097634a <a56097634a@gmail.com>
2015-11-10 19:22:31 -0500

    add dumb search and nearest facilities and info window on the map

--- Cryox.js
+++ Cryox.js
@@ -32,6 +32,7 @@ if (Meteor.isClient) {
 
     Template.destination.rendered = function() {
         Meteor.typeahead.inject();
+        GoogleMaps.load();
     };
 
     Template.destination.helpers({
@@ -47,8 +48,168 @@ if (Meteor.isClient) {
         'click button': function () {
             // decrement the counter when button is clicked
             Session.set('counter', Session.get('counter') - 1);
+        },
+        'click #dest': function(event){
+        	var value = event.currentTarget.value
+        	console.log('abc');
+        	Session.set('search', value);
         }
     });
+
+    /*
+     * Map
+     */
+
+     Template.maps.onCreated(function(){
+        GoogleMaps.ready('map', function(map){
+            console.log("Ready for action");
+            var geocoder = new google.maps.Geocoder;
+            var infowindow = new google.maps.InfoWindow;
+            //poly place
+            var placeId = "ChIJ85aDTUpawokR95FkWT0xm9o";
+            geocoder.geocode({'placeId': placeId}, function(results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    if (results[0]) {
+                        map.instance.setZoom(10);
+                        map.instance.setCenter(results[0].geometry.location);
+                        var marker = new google.maps.Marker({
+                            icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                            map: map.instance,
+                            position: results[0].geometry.location
+                        });
+                        infowindow.setContent("<div id=infowindow>"+results[0].formatted_address+"</div>");
+                        infowindow.open(map, marker);
+                        google.maps.event.addListener(marker, 'click', (function(marker) {
+                            	map.instance.setCenter(marker.getPosition());
+    							return function(){
+    								infowindow.setContent(results[0].formatted_address);
+    								infowindow.open(map.instance, marker);
+    							}
+  						})(marker)); 
+                    } else {
+                        window.alert('No results found');
+                    }
+                } else {
+                    window.alert('Geocoder failed due to: ' + status);
+                }
+            });
+            
+            var facilities = ["ChIJmQJIxlVYwokRLgeuocVOGVU", "ChIJR0lA1VBmwokR8BGfSBOyT-w", "ChIJxYx4g8pFwokRxOWbtEQbr3o", "ChIJm2RXxMxawokRuUokmugSnTE"];
+            for (var i = facilities.length - 1; i >= 0; i--) {
+                geocoder.geocode({'placeId': facilities[i]}, function(results, status){
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        if (results[0]) {
+                            var marker = new google.maps.Marker({
+                                map: map.instance,
+                                position: results[0].geometry.location
+                            });
+                            google.maps.event.addListener(marker, 'click', (function(marker) {
+                            	map.instance.setCenter(marker.getPosition());
+    							return function(){
+    								infowindow.setContent(results[0].formatted_address);
+    								infowindow.open(map.instance, marker);
+    							}
+  							})(marker)); 
+                        } else {
+                            window.alert('No results found');
+                        }
+                    } else {
+                        window.alert('Geocoder failed due to: ' + status);
+                    }
+                });
+            }
+        });
+    });
+
+     Template.maps.helpers({
+     	mapOptions: function(){
+     		if (GoogleMaps.loaded()) {
+      			return {
+        			center: {lat: 40.72, lng: -73.96},
+        			zoom: 8
+      			};
+    		}
+     	},
+     	search: function(){
+     		var s = Session.get('search');
+     		var searchID = "";
+     		var facilities = [];
+
+     		if (s == "Google Sydney, Darling Island Road, Pyrmont, New South Wales, Australia"){
+     			searchID = "ChIJs3u5LkiuEmsRnD4yjsR31dI";
+     			facilities = ["ChIJadrb42quEmsRIOvv-Wh9AQ8", "ChIJ3S-JXmauEmsRUcIaWtf4MzE", "ChIJ24MzG_GwEmsRd2VLWl01368", "ChIJ2fGeq9SxEmsRwAd6A2l9AR0"];
+     		}
+     		// num all the facilities and search possible
+     		else{
+     			return
+     		}
+
+     		GoogleMaps.ready('map', function(map){
+          		console.log("Ready for search");
+          		var geocoder = new google.maps.Geocoder;
+            	var infowindow = new google.maps.InfoWindow;
+
+            	geocoder.geocode({'placeId': searchID}, function(results, status) {
+                	if (status === google.maps.GeocoderStatus.OK) {
+                    	if (results[0]) {
+                        	map.instance.setZoom(10);
+                        	map.instance.setCenter(results[0].geometry.location);
+                        	var marker = new google.maps.Marker({
+                            	icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                            	map: map.instance,
+                            	position: results[0].geometry.location
+                        	});
+                        	infowindow.setContent("<div id=infowindow>"+results[0].formatted_address+"</div>");
+                        	infowindow.open(map, marker);
+                        	google.maps.event.addListener(marker, 'click', (function(marker) {
+                            	map.instance.setCenter(marker.getPosition());
+    							return function(){
+    								infowindow.setContent(results[0].formatted_address);
+    								infowindow.open(map.instance, marker);
+    							}
+  							})(marker)); 
+                    	} else {
+                        	window.alert('No results found');
+                    	}
+                	} else {
+                    	window.alert('Geocoder failed due to: ' + status);
+                	}
+            	});
+
+				for (var i = facilities.length - 1; i >= 0; i--) {
+                geocoder.geocode({'placeId': facilities[i]}, function(results, status){
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        if (results[0]) {
+                            var marker = new google.maps.Marker({
+                                map: map.instance,
+                                position: results[0].geometry.location
+                            });
+                            google.maps.event.addListener(marker, 'click', (function(marker) {
+                            	map.instance.setCenter(marker.getPosition());
+    							return function(){
+    								infowindow.setContent(results[0].formatted_address);
+    								infowindow.open(map.instance, marker);
+    							}
+  							})(marker)); 
+                        } else {
+                            window.alert('No results found');
+                        }
+                    } else {
+                        window.alert('Geocoder failed due to: ' + status);
+                    }
+                });
+            }
+
+    	});
+
+     	}
+ 	});
+
+     Template.maps.events({
+     	'click': function(){
+
+     	}
+     });
 
     /*
      * Progress
@@ -79,7 +240,6 @@ Destinations = new Meteor.Collection("destinations");
 
 if (Meteor.isServer){
     Meteor.startup(function () {
-
         function fill(col, source, map){
             col.remove({});
             JSON.parse(Assets.getText(source)).forEach(function(it){
